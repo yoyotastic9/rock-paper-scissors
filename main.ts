c0d293d51f1d2027d72057c00714a8e3e55838b0
yoyotastic9 <76183586+yoyotastic9@users.noreply.github.com>
2020-12-30 20:37:30 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let HAND = 0
+input.onGesture(Gesture.Shake, function () {
+    HAND = randint(1, 3)
+    if (HAND == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (HAND == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
